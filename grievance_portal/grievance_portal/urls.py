699f17eafcf4dd9d04202a990edef8d1c5c7d900
monika <monika.pandey97@gmail.com>
2019-09-15 00:29:58 +0530

    first commit

---
+++ grievance_portal/grievance_portal/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+from django.contrib import admin
+
+urlpatterns = [url(r'^admin/', include(admin.site.urls))
+    # Examples:
+    # url(r'^$', 'grievance_portal.views.home', name='home'),
+    # url(r'^grievance_portal/', include('grievance_portal.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
